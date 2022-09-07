445e7679ebb886ad11f7b596eb17f6f8c212ada3
KittinanPog <89690147+KittinanPog@users.noreply.github.com>
2022-09-07 12:45:28 +0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB203_64
+ */
+public class Customer{
+    private String firstname;
+    private String lastname;
+    private CheckingAccount acct;
+    
+    public Customer(){
+        
+    }
+    public Customer(String firstname, String lastname){
+        
+    }
+    public Customer(String firstname, String lastname, CheckingAccount acct){
+        
+    }
+    public void setFirstName(String firstname){
+        
+    }
+    public String getFirstName(){
+        
+    }
+    public void setLastName(String lastname){
+        
+    }
+    public String getLastName(){
+        
+    }
+    public 
+}
---
+++ Main1.java
@@ -0,0 +1,8 @@
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        Account a1 = new Account();
+        a1.showAccount();
+    }
+}
